1a50708fad5af3dd344d5141cd3a7f3b137d256d
nichol6s <nicholas.jake15@gmail.com>
2024-04-02 20:09:21 -0300

    Instalando a fonte no app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,31 @@
 import "../styles/global.css"
 
 import { Slot } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
-export default function Layout(){
-    return(
-        <Slot/>
+import {
+    useFonts,
+    Roboto_700Bold,
+    Roboto_500Medium,
+    Roboto_400Regular
+} from "@expo-google-fonts/roboto"
+
+export default function Layout() {
+
+    const [fontsLoaded] = useFonts({
+        Roboto_700Bold,
+        Roboto_500Medium,
+        Roboto_400Regular
+    })
+
+    if(!fontsLoaded){
+        return;
+    }
+
+    return (
+        <>
+            <StatusBar style="light" />
+            <Slot />
+        </>
     )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text, } from "react-native"
 
 export default function Home(){
     return(
-        <View>
-            <Text>Oiiiiiiiiiii</Text>
+        <View className="flex-1 bg-green-500 items-center justify-center">
+            <Text className="text-white font-bold">Oiiiiiiiiiii</Text>
         </View>
     )
 }
