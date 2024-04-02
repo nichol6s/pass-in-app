2aad76289f696d49003f29e69dab9234cc0c4d29
nichol6s <nicholas.jake15@gmail.com>
2024-04-02 20:21:11 -0300

    Criando o componente Loading e passando a usar importações dinâmicas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
-import "../styles/global.css"
+import "@/styles/global.css"
 
 import { Slot } from "expo-router"
 import { StatusBar } from "expo-status-bar"
+
+import { Loading } from "@/components/loading"
 
 import {
     useFonts,
@@ -18,14 +20,10 @@ export default function Layout() {
         Roboto_400Regular
     })
 
-    if(!fontsLoaded){
-        return;
-    }
-
     return (
         <>
             <StatusBar style="light" />
-            <Slot />
+            { fontsLoaded ? <Slot /> : <Loading />}
         </>
     )
 }
