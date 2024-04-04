b715f7647ea7912470cec5c771c205a0622938fb
nichol6s <nicholas.jake15@gmail.com>
2024-04-04 19:09:02 -0300

    Usando useState na pagina index e register

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,7 @@
-import { View, Image, StatusBar } from "react-native"
-import {  MaterialCommunityIcons } from "@expo/vector-icons"
+import { useState } from "react"
+
+import { View, Image, StatusBar, Alert } from "react-native"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 import { Link } from "expo-router"
 
 import { Input } from "@/components/input"
@@ -8,6 +10,15 @@ import { Button } from "@/components/button"
 import { colors } from "@/styles/colors"
 
 export default function Home() {
+
+    const [code, setCode] = useState("")
+
+    function handleAcessCredential(){
+        if(!code.trim()){
+            return Alert.alert("Credencial", "Informe o código do ingresso!")
+        }
+    }
+    
     return (
         <View className="flex-1 bg-green-500 items-center justify-center p-8">
             <StatusBar barStyle="light-content" />
@@ -19,17 +30,20 @@ export default function Home() {
 
             <View className="w-full mt-12 gap-3">
                 <Input>
-                    <MaterialCommunityIcons 
-                    name="ticket-confirmation-outline" 
-                    color={colors.green[200]}
-                    size={20} 
+                    <MaterialCommunityIcons
+                        name="ticket-confirmation-outline"
+                        color={colors.green[200]}
+                        size={20}
                     />
-                    <Input.Field placeholder="Código do ingresso" />
+                    <Input.Field
+                        placeholder="Código do ingresso"
+                        onChangeText={setCode}
+                    />
                 </Input>
 
-                <Button title="Acessar credencial"/>
+                <Button title="Acessar credencial" onPress={handleAcessCredential} />
 
-                <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8" >Ainda não possui ingresso?</Link>
+                <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8" > Ainda não possui ingresso? </Link>
             </View>
 
         </View>
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,6 +1,8 @@
-import { View, Image, StatusBar } from "react-native"
+import { useState } from "react"
+
+import { View, Image, StatusBar, Alert } from "react-native"
 import {  FontAwesome6, MaterialIcons } from "@expo/vector-icons"
-import { Link } from "expo-router"
+import { Link, router } from "expo-router"
 
 import { Input } from "@/components/input"
 import { Button } from "@/components/button"
@@ -8,6 +10,18 @@ import { Button } from "@/components/button"
 import { colors } from "@/styles/colors"
 
 export default function Register() {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    function handleRegister(){
+        if(!name.trim() || !email.trim()){
+            return Alert.alert("Inscrição", "Preencha todos os campos!")
+        }
+
+        router.push("/ticket")
+    }
+
     return (
         <View className="flex-1 bg-green-500 items-center justify-center p-8">
             <StatusBar barStyle="light-content" />
@@ -24,7 +38,7 @@ export default function Register() {
                     color={colors.green[200]}
                     size={20} 
                     />
-                    <Input.Field placeholder="Nome completo" />
+                    <Input.Field placeholder="Nome completo" onChangeText={setName} />
                 </Input>
 
                 <Input>
@@ -33,10 +47,10 @@ export default function Register() {
                     color={colors.green[200]}
                     size={20} 
                     />
-                    <Input.Field placeholder="E-mail" keyboardType="email-address" />
+                    <Input.Field placeholder="E-mail" keyboardType="email-address" onChangeText={setEmail} />
                 </Input>
 
-                <Button title="Realizar inscrição"/>
+                <Button title="Realizar inscrição" onPress={handleRegister} />
 
                 <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8" >Ja possui ingresso?</Link>
             </View>
