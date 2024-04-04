ffbfcb3f886611f0e56205442028a2541c2d765f
nichol6s <nicholas.jake15@gmail.com>
2024-04-04 19:09:29 -0300

    Pagina ticket criada, componente header finalizado, terminando o credential

---
+++ src/app/ticket.tsx
@@ -0,0 +1,43 @@
+import { View, StatusBar, Text, ScrollView, TouchableOpacity, StyleSheet } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'
+
+import { Header } from "@/components/header";
+import { Credential } from "@/components/credential";
+
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
+
+export default function Ticket() {
+    return (
+        <View className="flex-1 bg-green-500">
+            <StatusBar barStyle="light-content" />
+            <Header title="Minha credencial" />
+
+            <ScrollView className="-mt-28 -z-10" contentContainerClassName="px-8 pb-8" showsVerticalScrollIndicator={false}>
+                <Credential />
+
+                <FontAwesome
+                    name="angle-double-down"
+                    size={24}
+                    color={colors.gray[300]}
+                    className="self-center my-6"
+                />
+
+                <Text className="text-white font-bold text-2xl mt-4">Compartilhar Credencial</Text>
+                <Text className="text-white font-regular text-base mt-1 mb-6">Mostre ao mundo que você vai participar do Unite Summit</Text>
+
+                <Button title="Compartilhar"/>
+
+                <TouchableOpacity activeOpacity={0.7} style={styles.touchable} >
+                    <Text className="text-base text-white font-bold text-center">Remover Ingresso</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    touchable:{
+        marginTop: 30
+    }
+})
