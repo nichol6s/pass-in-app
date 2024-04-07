77f161392a078d48c045abb0a799d39a0475561b
nichol6s <nicholas.jake15@gmail.com>
2024-04-06 22:43:12 -0300

    Fazendo uma animação do Crachá usando Reanimated com Moti

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,6 +1,7 @@
-import { View, Image, ImageBackground, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Image, ImageBackground, Text, TouchableOpacity, StyleSheet, useWindowDimensions } from "react-native";
 
 import { Feather } from '@expo/vector-icons'
+import { MotiView } from 'moti'
 
 import { QRCode } from "@/components/qrcode";
 
@@ -16,8 +17,34 @@ type Props = {
 }
 
 export function Credential({ data, onChangeAvatar, onExpandQRCode }: Props) {
+    const { height } = useWindowDimensions()
+
     return (
-        <View className="w-full self-stretch items-center">
+        <MotiView 
+        className="w-full self-stretch items-center"
+        from={{ 
+            opacity: 0, 
+            translateY: -height,
+            rotateZ: "40deg",
+            rotateY: "30deg",
+            rotateX: "30deg"
+        }}  
+        animate={{
+            opacity: 1,
+            translateY: 0,
+            rotateZ: "0deg",
+            rotateY: "0deg",
+            rotateX: "0deg",
+        }}
+        transition={{
+            type: "spring",
+            damping: 20,
+            rotateZ: {
+                damping: 15,
+                mass: 3
+            }
+        }}
+        >
             <Image source={require("@/assets/ticket/band.png")} className="w-24 h-52 z-10" />
 
             <View className="bg-black/20 self-stretch items-center pb-6 border border-white/10 mx-3 rounded-2xl -mt-5">
@@ -64,7 +91,7 @@ export function Credential({ data, onChangeAvatar, onExpandQRCode }: Props) {
                 </TouchableOpacity>
             </View>
 
-        </View>
+        </MotiView>
     )
 }
 
